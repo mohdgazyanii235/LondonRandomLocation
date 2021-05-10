dd3ddc8f0b0649a4c097b010676f124a2ff3e5d6
mohdgazyanii235 <mohdgazyanii235@users.noreply.github.com>
2021-05-10 14:49:59 +0100

    gitignore

--- app.py
+++ app.py
@@ -3,34 +3,32 @@ import random
 import linecache
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
-import jinja2
 
 app = Flask(__name__)
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyB2gWJN7HHCh-y15I2kw1A5PqOLU22l3z8"
 GoogleMaps(app)
 
-
-
+def distance_calculator(latitude, )
 
 def get_random_pcd():
-    rand_line_num = random.randint(0 , 321376)
-    return linecache.getline("All London PCDs.csv" , rand_line_num)
+    rand_line_num = random.randint(0, 321376)
+    return linecache.getline("All London PCDs.csv", rand_line_num)
 
 
-@app.route('/' , methods=["GET" , "POST"])
+@app.route('/', methods=["GET", "POST"])
 def home():
     random_pcd = get_random_pcd()
     if request.method == "POST":
         random_pcd = get_random_pcd()
 
     google_map = Map(
-        identifier="view-side" ,  # for DOM element
-        varname="mymap" ,  # for JS object name
-        lat=37.4419 ,
-        lng=-122.1419 ,
+        identifier="view-side",  # for DOM element
+        varname="mymap",  # for JS object name
+        lat=37.4419,
+        lng=-122.1419,
     )
-    return render_template("homepage.html" , random_pcd=random_pcd, mymap=google_map)
+    return render_template("homepage.html", random_pcd=random_pcd, mymap=google_map)
 
 
 if __name__ == '__main__':
-    app.run(debug=True , host='127.0.0.1' , port=8000)
+    app.run(debug=True, host='127.0.0.1', port=8000)
