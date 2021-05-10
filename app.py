78f3632b1f1a2267abff4bd952599850001c1ca5
mohdgazyanii235 <mohdgazyanii235@users.noreply.github.com>
2021-05-10 15:42:54 +0100

    min station function

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template, send_file, request, url_for, redirect, flash, session, Markup
+from flask import Flask , render_template , send_file , request , url_for , redirect , flash , session , Markup
 import random
 import linecache
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
+import math
 
 
 app = Flask(__name__)
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyB2gWJN7HHCh-y15I2kw1A5PqOLU22l3z8"
 GoogleMaps(app)
+
+
+def distance_calculator(latitude, longitude):
+    min_distance = 99999999999
+    min_station = ""
+    with open("D:\LondonRandomLocation\AllLondonStations.csv", "r") as station_file:
+        for line in station_file:
+            station_name = line.split(",")[0]
+            station_lat = float(line.split(",")[1])
+            station_long = float(line.split(",")[2])
+            calc_distance = math.sqrt((station_lat - latitude)**2 + (station_long-longitude)**2)
+            if calc_distance < min_distance:
+                min_distance = calc_distance
+                min_station = station_name
+    return min_station
 
 
 def get_random_pcd():
@@ -20,8 +36,9 @@ def home():
     random_pcd = get_random_pcd()
     if request.method == "POST":
         random_pcd = get_random_pcd()
-
-    return render_template("homepage.html", random_pcd=random_pcd.split(","))
+    random_pcd = random_pcd.split(",")
+    print("The closes tube station is: " + distance_calculator(float(random_pcd[1]), float(random_pcd[2])))
+    return render_template("homepage.html", random_pcd=random_pcd)
 
 
 if __name__ == '__main__':
