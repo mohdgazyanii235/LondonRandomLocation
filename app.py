364e9010ed0422fbf6e1d78f90fd4cbfd17129f1
Zinks2 <75337511+Zinks2@users.noreply.github.com>
2021-05-10 14:15:04 +0100

    Uploaded start of project

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask , render_template , send_file , request , url_for , redirect , flash , session , Markup
+import random
+import linecache
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+import jinja2
+
+app = Flask(__name__)
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyBRL6p6u0r-p8-Bl9FXZb-PrzOQrecxPFE"
+GoogleMaps(app)
+
+
+
+
+def get_random_pcd():
+    rand_line_num = random.randint(0 , 321376)
+    return linecache.getline("All London PCDs.csv" , rand_line_num)
+
+
+@app.route('/' , methods=["GET" , "POST"])
+def home():
+    random_pcd = get_random_pcd()
+    if request.method == "POST":
+        random_pcd = get_random_pcd()
+
+    google_map = Map(
+        identifier="view-side" ,  # for DOM element
+        varname="mymap" ,  # for JS object name
+        lat=37.4419 ,
+        lng=-122.1419 ,
+    )
+    return render_template("homepage.html" , random_pcd=random_pcd, mymap=google_map)
+
+
+if __name__ == '__main__':
+    app.run(debug=True , host='127.0.0.1' , port=8000)
