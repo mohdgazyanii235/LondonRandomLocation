745a806c9b64191163abf6f01ea8059868dda4fb
Lucas <Lucas.kimber@outlook.com>
2021-05-11 23:07:54 +0100

    Added base template with nav bar and map formatted

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask , render_template , send_file , request , url_for , redirect , flash , session , Markup
+from flask import Flask, render_template, send_file, request, url_for, redirect, flash, session, Markup
 import random
 import linecache
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 import math
+from flask_bootstrap5 import Bootstrap
 
 
 app = Flask(__name__)
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyB2gWJN7HHCh-y15I2kw1A5PqOLU22l3z8"
 GoogleMaps(app)
+bootstrap = Bootstrap(app)
 
 
 def distance_calculator(latitude, longitude):
@@ -38,7 +40,7 @@ def home():
         random_pcd = get_random_pcd()
     random_pcd = random_pcd.split(",")
     print("The closes tube station is: " + distance_calculator(float(random_pcd[1]), float(random_pcd[2])))
-    return render_template("homepage.html", random_pcd=random_pcd)
+    return render_template("description_page.html", random_pcd=random_pcd)
 
 
 if __name__ == '__main__':
